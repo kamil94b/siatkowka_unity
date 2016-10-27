dfe9e0265c5a5d5e1b3250ec3b76e8e63318747b
pklasa <klasappiotr@wp.pl>
2016-10-27 15:15:31 +0200

    troche bardziej grywalna wersja fizyki
    jeszcze komputer nie za bardzo dizala

--- Siatkowka 3D/Assets/ruch.cs
+++ Siatkowka 3D/Assets/ruch.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class ruch : MonoBehaviour {
 
 	//Obiekt odpowiedzialny za ruch gracza.
-	private CharacterController characterControler;
+	//private CharacterController characterControler;
+	private Rigidbody rb;
 	public pilka pilka;
 
 	private float predkoscPoruszania = 9.0f;
@@ -17,9 +18,17 @@ public class ruch : MonoBehaviour {
 
 	public bool isComputerPlayer = false;
 
+	private float ziemia = 6.62f;
+	private float jumpSpeed = 0.0f;
+
+
+	public float ogranicznikPolaLeft;
+	public float ogranicznikPolaRight;
+	public float ogranicznikPolaTop;
+	public float ogranicznikPolaBottom;
 
 	void Start () {
-		characterControler = GetComponent<CharacterController>();
+		rb = GetComponent<Rigidbody>();
 	}
 
 
@@ -33,21 +42,46 @@ public class ruch : MonoBehaviour {
 			float rochLewoPrawo = Input.GetAxis ("Horizontal") * predkoscPoruszania;
 
 
-			if (characterControler.isGrounded && Input.GetButton ("Jump")) {
-				aktualnaWysokoscSkoku = wysokoscSkoku;
-			} else if (!characterControler.isGrounded) {
-				if (Input.GetButton ("Jump")) {
-					aktualnaWysokoscSkoku += Physics.gravity.y * Time.deltaTime * 1.5f;
-				} else {
-					aktualnaWysokoscSkoku += Physics.gravity.y * Time.deltaTime * 3;
-				}
+			if (transform.position.z > polowaBoiskaZ) {
+				rochPrzodTyl = rochPrzodTyl * -1.0f;
+				rochLewoPrawo = rochLewoPrawo*-1.0f;
 			}
 
-			Vector3 ruch = new Vector3 (rochLewoPrawo, aktualnaWysokoscSkoku, rochPrzodTyl);
+			if (rb.position.x < ogranicznikPolaLeft + 1.1f && rochLewoPrawo < 0) {
+				rochLewoPrawo = 0;
+			}
+			if (rb.position.x > ogranicznikPolaRight - 1.1f && rochLewoPrawo > 0) {
+				rochLewoPrawo = 0;
+			}
+			if (rb.position.z > ogranicznikPolaTop -1.1f  && rochPrzodTyl > 0) {
+				rochPrzodTyl = 0;
+			}
+			if (rb.position.z < ogranicznikPolaBottom + 1.1f && rochPrzodTyl < 0) {
+				rochPrzodTyl = 0;
+			}
+
+			aktualnaWysokoscSkoku = 0;
+
+
+			if (Input.GetButton ("Jump")) {
+				if (rb.position.y < ziemia + 0.5) {
+					jumpSpeed = 10;
+				} else {
+					jumpSpeed = rb.velocity.y + Physics.gravity.y * Time.deltaTime * 1.2f;
+				}
+			} else {
+				jumpSpeed = rb.velocity.y + Physics.gravity.y * Time.deltaTime * 2.0f;
+			}
+
+				
+			//jumpSpeed *= 1.5f;
+			Vector3 ruch = new Vector3 (rochLewoPrawo, jumpSpeed, rochPrzodTyl);
+
+			rb.velocity = ruch;
 
 			//ruch = transform.rotation * ruch;
 
-			characterControler.Move (ruch * Time.deltaTime);
+			//characterControler.Move (ruch * Time.deltaTime);
 		} else {
 
 			//komputer
@@ -82,6 +116,10 @@ public class ruch : MonoBehaviour {
 			//	ruch = new Vector3(celZ - transform.position.z,0,0);
 			//}
 
+
+
+			Vector3 ruch = new Vector3 (getMoveFromTo(rb.position.x,celX,30.0f), 0.0f, getMoveFromTo(rb.position.z,celZ,30.0f));
+			rb.velocity = ruch;
 
 			//transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z+);
 			//c/haracterControler.Move (ruch);
