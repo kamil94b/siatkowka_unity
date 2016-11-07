88555d165d865868d67c96d5459a3b09f3e3007b
Paweł Babicz <babiczp@student.agh.edu.pl>
2016-11-07 02:59:41 +0100

    Area and menu

---
+++ Siatkowka 3D/Assets/Scripts/SingleOrMulti.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SingleOrMulti : MonoBehaviour {
+    
+    public bool SelectSingle(bool choice)
+    {
+        Application.LoadLevel(1);
+
+        bool SingleSelect = choice;
+        return SingleSelect;
+    }
+}
